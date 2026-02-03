b7c29c143263a78decff202e484b6c517def1dfd
Unuku05 <143775909+Unuku05@users.noreply.github.com>
2026-02-03 11:04:58 +0800

    ok

--- App/App.tsx
+++ App/App.tsx
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
